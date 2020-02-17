50413ff83bda2d83ae41c795446342f98fc86758
Jarod Peachey <jwpeachey107@aol.com>
2020-02-17 09:42:29 -0500

    Get the API working

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,97 @@ import { checkForName } from './js/nameChecker';
 import { handleSubmit } from './js/formHandler';
 import './styles/main.scss';
 
-console.log(checkForName);
-console.log('CHANGE!!');
+const urlInput = document.getElementById('url');
+const submitButton = document.getElementById('submit');
+
+// Event Listener
+submitButton.addEventListener('click', submitUrl);
+
+// Add Entry
+async function submitUrl(e) {
+  e.preventDefault();
+  if (urlInput.value == '') {
+    alert('Please fill in all the fields.');
+  } else {
+    const postResult = await postUrlRequest('/api', urlInput.value);
+
+    const getResult = await getData('/get', getData);
+
+    console.log(getResult);
+  }
+}
+
+async function postUrlRequest(urlToRequestTo, urlForApi) {
+  // Create a new date instance dynamically with JS
+  let d = new Date();
+  let newDate = d.getTime();
+
+  const data = {
+    url: urlForApi,
+    date: newDate,
+  };
+
+  let result = await fetch(urlToRequestTo, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  let responseData = await result.json();
+
+  return responseData;
+}
+
+async function getData(url) {
+  let result = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  let responseData = await result.json();
+
+  return responseData;
+}
+
+function displayResults(data) {
+  const entries = data.entries;
+  const { name, date, feeling, zip, temp } = entries[entries.length - 1];
+
+  const wrapper = document.createElement('div');
+  const flex = document.createElement('div');
+  const textWrapper = document.createElement('div');
+  const nameElement = document.createElement('em');
+  const dateElement = document.createElement('span');
+  const tempElement = document.createElement('div');
+  const tempIndicator = document.createElement('span');
+  const feelingsElement = document.createElement('p');
+
+  wrapper.classList.add('entry-card');
+  flex.classList.add('entry-flex');
+  nameElement.classList.add('entry-name');
+  dateElement.classList.add('entry-date');
+  tempElement.classList.add('entry-temp');
+  tempIndicator.classList.add('entry-temp-indicator');
+  textWrapper.classList.add('entry-feelings');
+
+  nameElement.innerText = `- ${name}`;
+  dateElement.innerText = date;
+  feelingsElement.innerText = feeling;
+  tempIndicator.innerText = '(F)';
+  tempElement.innerText = `${temp}°`;
+  tempElement.appendChild(tempIndicator);
+
+  flex.appendChild(tempElement);
+  flex.appendChild(dateElement);
+  textWrapper.appendChild(feelingsElement);
+  textWrapper.appendChild(nameElement);
+  wrapper.appendChild(flex);
+  wrapper.appendChild(textWrapper);
+
+  entriesElement.appendChild(wrapper);
+  entrySection.style.display = 'block';
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,35 +1,77 @@
-// Environment variables
+// Dependencies
 const dotenv = require('dotenv');
-dotenv.config();
-
-var path = require('path');
 const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
 const mockAPIResponse = require('./mockAPI.js');
 const aylien = require('aylien_textapi');
+
+dotenv.config();
 
 // Initiate server
 const app = express();
 
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware, as well as cors
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
 // Use dist folder
 app.use(express.static('dist'));
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function() {
-  console.log('Example app listening on port 8080!');
+// Initialize server
+const port = 3000;
+
+const server = app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
 });
 
 // Initiate Aylien
-const textApi = new aylien({
+const api = new aylien({
   application_id: process.env.API_ID,
   application_key: process.env.API_KEY,
 });
+
+// Empty data object
+projectData = {
+  entries: [],
+};
 
 // Get function
 app.get('/', function(req, res) {
   res.sendFile('dist/index.html');
 });
 
-// Another get function
-app.get('/test', function(req, res) {
-  res.send(mockAPIResponse);
-});
+app.get('/get', sendData);
+app.post('/api', makeApiRequest);
+
+function sendData(request, response) {
+  response.send(projectData);
+}
+
+async function makeApiRequest(request, response) {
+  // fetch(process.env.API_ENDPOINT)
+  const apiResponse = await api.sentiment(
+    {
+      url: request.body.url,
+    },
+    (err, res) => {
+      const newEntry = {
+        ...res,
+        date: request.body.date,
+      };
+
+      postNewEntry(newEntry);
+      response.send(newEntry);
+    },
+  );
+}
+
+function postNewEntry(newEntry) {
+  projectData = {
+    entries: [...projectData.entries, newEntry],
+  };
+}
